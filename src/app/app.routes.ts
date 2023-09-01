02e93c7714b5cd4ae59cd11e0ca9fb3bbd478e42
Gonzalo <go.ulloa@duocuc.cl>
2023-09-01 14:58:43 -0400

    funcionalidad login y vista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
+    path: '**',
     redirectTo: 'login',
     pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
-
+  }
 ];
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,25 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { Route, Router } from '@angular/router';
+import { Router, NavigationExtras, RouterLinkWithHref } from '@angular/router';
+import { IUserLogin } from '../models/IUserLogin';
+import { UserModel } from 'src/app/models/UserModel';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
-})
+  imports: [IonicModule, CommonModule, FormsModule,ReactiveFormsModule,
+    RouterLinkWithHref, FormsModule, RouterModule]})
 export class LoginPage implements OnInit {
 
+  listUser: UserModel[] = [
+    new UserModel('Gonzalo','Ulloa','go.ulloa@duocuc.cl','PROFESOR','go.ulloa','ulloa123'),
+    new UserModel('David','Mallma','da.mallma@duocuc.cl','ALUMNO','da.mallma','mallma123')
+  ];
+
+  userLoginModal: IUserLogin = {
+    username: '',
+    password: ''
+  };
+  
   constructor(private router: Router){}
 
   ngOnInit() {
+    this.userLoginModalRestart();
   }
 
   gotoHome(){
     this.router.navigate(['/home'])
+  }
+  
+  userLogin(userLoginInfo: IUserLogin): boolean{
+    for(let i = 0; i < this.listUser.length; i++){
+      console.log('Comparando:', this.listUser[i].username, userLoginInfo.username);
+      console.log('Contraseñas:', this.listUser[i].password, userLoginInfo.password);
+      if((this.listUser[i].username == userLoginInfo.username) && (this.listUser[i].password == userLoginInfo.password)){
+        console.log('User Loged...', this.userLoginModal.username, this.userLoginModal.password);
+        let userInfoSend: NavigationExtras = {
+          state: {
+            user: this.listUser[i]
+          }
+        }
+        let sendInfo = this.router.navigate(['/home'], userInfoSend);
+      }
+      else{
+        console.log('User not found');
+      }
+    }
+    this.userLoginModalRestart();
+    return false;
+    
+  }
+
+  userLoginModalRestart(): void{
+    this.userLoginModal.username = '';
+    this.userLoginModal.password = '';
   }
 
 }
