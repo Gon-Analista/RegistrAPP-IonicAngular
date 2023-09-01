e942ee93b1926ac8d505c0c0a0c783f274a04489
Gonzalo <go.ulloa@duocuc.cl>
2023-09-01 18:42:50 -0400

    arreglo css perfil y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
+  {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
