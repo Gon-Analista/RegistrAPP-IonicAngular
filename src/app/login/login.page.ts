70e948b0b0532019758ec7597391375d698bb81c
Gonzalo <go.ulloa@duocuc.cl>
2023-09-03 17:26:37 -0300

    Nuevo login con map y alertas de david

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, AlertController } from '@ionic/angular';
 import { Router, NavigationExtras, RouterLinkWithHref } from '@angular/router';
 import { IUserLogin } from '../models/IUserLogin';
 import { UserModel } from 'src/app/models/UserModel';
@@ -12,58 +12,72 @@ import { RouterModule } from '@angular/router';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule,ReactiveFormsModule,
-    RouterLinkWithHref, FormsModule, RouterModule]})
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule, RouterLinkWithHref, FormsModule, RouterModule]
+})
 export class LoginPage implements OnInit {
-
-  listUser: UserModel[] = [
-    new UserModel('Gonzalo','Ulloa','go.ulloa@duocuc.cl','PROFESOR','go.ulloa','ulloa123'),
-    new UserModel('David','Mallma','da.mallma@duocuc.cl','ALUMNO','da.mallma','mallma123')
-  ];
+  usersMap: Map<string, UserModel>;
 
   userLoginModal: IUserLogin = {
     username: '',
     password: ''
   };
-  
-  constructor(private router: Router){}
+
+  constructor(private router: Router, private alertController: AlertController) {
+    this.usersMap = new Map<string, UserModel>();
+    this.usersMap.set('go.ulloa', new UserModel('Gonzalo', 'Ulloa', 'go.ulloa@duocuc.cl', 'PROFESOR', 'go.ulloa', 'ulloa123'));
+    this.usersMap.set('da.mallma', new UserModel('David', 'Mallma', 'da.mallma@duocuc.cl', 'ALUMNO', 'da.mallma', 'mallma123'));
+  }
 
   ngOnInit() {
     this.userLoginModalRestart();
   }
 
-  gotoPerfil(){
+  gotoPerfil() {
     this.router.navigate(['/perfil'])
   }
-  
-  userLogin(userLoginInfo: IUserLogin): boolean{
-    for(let i = 0; i < this.listUser.length; i++){
-      console.log('Comparando:', this.listUser[i].username, userLoginInfo.username);
-      console.log('Contraseñas:', this.listUser[i].password, userLoginInfo.password);
-      if((this.listUser[i].username == userLoginInfo.username) && (this.listUser[i].password == userLoginInfo.password)){
-        console.log('User Loged...', this.userLoginModal.username, this.userLoginModal.password);
-        let userInfoSend: NavigationExtras = {
-          state: {
-            user: this.listUser[i]
-          }
-        }
-        let sendInfo = this.router.navigate(['/perfil'], userInfoSend);
-      }
-      else{
-        console.log('User not found');
-      }
+
+  async userLogin(userLoginInfo: IUserLogin): Promise<void> {
+    const user = this.usersMap.get(userLoginInfo.username);
+    if (!userLoginInfo.username || !userLoginInfo.password) {
+      // Campos en blanco, muestra la alerta
+      await this.showAlert('Campos en blanco', 'Por favor, complete ambos campos.');
+    } else if (user && user.password === userLoginInfo.password) {
+      console.log('User Loged...', user.username, user.password);
+      const userInfoSend: NavigationExtras = {
+        state: {
+          user,
+        },
+      };
+      this.router.navigate(['/perfil'], userInfoSend);
+    } else {
+      // Usuario no encontrado o contraseña incorrecta, muestra la alerta
+      await this.showAlert('Credenciales incorrectas', 'El nombre de usuario o la contraseña son incorrectos.');
     }
+
     this.userLoginModalRestart();
-    return false;
-    
   }
 
-  userLoginModalRestart(): void{
+  userLoginModalRestart(): void {
     this.userLoginModal.username = '';
     this.userLoginModal.password = '';
   }
-  gotoRest(){
-    this.router.navigate(['/restablecer'])
+
+  gotoRest() {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        usersMap: this.usersMap, // Pasa el mapa de usuarios a la página de restablecimiento
+      },
+    };
+    this.router.navigate(['/restablecer'], navigationExtras);
   }
 
+  async showAlert(header: string, message: string): Promise<void> {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
