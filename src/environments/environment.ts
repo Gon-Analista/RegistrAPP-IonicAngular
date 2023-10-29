4e1fef3bf84c1bc23ed4f02e4e5497672713c725
Gonzalo <go.ulloa@duocuc.cl>
2023-10-29 01:15:29 -0300

    bd environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  supabaseUrl: 'https://pluhkyjkzcqesuwnvwmh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsdWhreWpremNxZXN1d252d21oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc4OTk0MjQsImV4cCI6MjAxMzQ3NTQyNH0.zg4cfELCrPGkXCeGWp8O7tOQoOSqojtT4r5GMjGE2dU',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
